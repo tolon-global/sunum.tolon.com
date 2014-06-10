54569b5bbc661469f6ce2a8fa6453bf284d2b6ca
Mert Torun <mert@merttorun.com>
2014-06-10 13:35:51 +0300

    switch to custom zoomin plugin

--- plugin/zoom-js/zoom.js
+++ plugin/zoom-js/zoom.js
@@ -1,43 +1,5 @@
 // Custom reveal.js integration
-(function(){
-	var isEnabled = true;
 
-	Reveal.addEventListener( 'fragmentshown', function( event ) {
-		if( isEnabled ) {
-            var waitForZoomOut = 0;
-			event.preventDefault();
-            zoomTo(event.fragment);
-		}
-	} );
-    
-    Reveal.addEventListener( 'slidechanged', function( event ) { 
-        if(event.currentSlide.dataset.flow==null) { 
-            zoomTo(false);
-        } else {
-            var firstFragment = event.currentSlide.querySelector("div:first-of-type");
-            zoomTo(firstFragment);
-        }
-    } );
-    
-	Reveal.addEventListener( 'overviewshown', function() { isEnabled = false; } );
-	Reveal.addEventListener( 'overviewhidden', function() { isEnabled = true; } );
-    
-    function zoomTo(element) {
-        var tr = {}
-        if (element) {
-            tr = { 
-                    x: element.offsetLeft + element.offsetWidth/2, 
-                    y: element.offsetTop + element.offsetHeight/2, 
-                    scale: 3
-            };
-        } else {
-            tr = { x: 0, y: 0, scale: 1 };
-        }
-        document.body.style.webkitTransformOrigin = tr.x + 'px ' + tr.y + 'px';
-        document.body.style.webkitTransform = 'scale(' + tr.scale + ')';
-    }
-    
-})();
 
 /*!
  * zoom.js 0.3
